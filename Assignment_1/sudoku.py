0e96cb59f3a1f088e07a4b167dcbf5637669ddae
dat.huynh11082001@hcmut.edu.vn <dat.huynh11082001@hcmut.edu.vn>
2022-03-19 20:34:48 +0700

    First commit

--- Assignment_1/sudoku.py
+++ Assignment_1/sudoku.py
@@ -2,7 +2,7 @@
 from math import sqrt
 import copy
 
-class Board:
+class Sudoku:
     def __init__(self, board, type = "online"):
         if type == "online":
             self.board = []
@@ -20,10 +20,9 @@ class Board:
             for i in range(0, len(self.board)):
                 board[i] = list(map(lambda c: 0 if c == "." else int(c), board[i]))
         self.square_size = int(sqrt(self.size))
-        self.history = []
 
     def __str__ (self):
-        return Board.board_to_string(self.board, self.size)
+        return Sudoku.board_to_string(self.board, self.size)
     
     @staticmethod    
     def board_to_string(board, size):
@@ -36,24 +35,25 @@ class Board:
             result+= "\n"
             if ((i + 1) % square_size == 0 and i < size - 1): result += "---------------------\n"
         return result
-
-    def result(self):
+    
+    @staticmethod
+    def result(state, history, size):
         result = ""
-        for history in self.history:
-            result += Board.board_to_string(history[3], self.size) + "\n"+ f"Row: {history[0]}, Column: {history[1]}, Value: {history[2]}\n"
-        result += self.__str__()
+        for history_state in history:
+            result += Sudoku.board_to_string(history_state[3], size) + "\n"+ f"Row: {history_state[0]}, Column: {history_state[1]}, Value: {history_state[2]}\n"
+        result += Sudoku.board_to_string(state, size)
         return result
     
-    def check_goal(self):
+    def check_goal(self, state):
         expect = sum(range(1, self.size + 1))
         # Check row and column:
         for i in range(0, self.size):
-            if 0 in self.board[i]: return False
-            if sum(self.board[i]) != expect: return False
+            if 0 in state[i]: return False
+            if sum(state[i]) != expect: return False
             sum_column = 0
             for j in range(0, self.size):
-                if self.board[j][i] == 0: return False
-                sum_column += self.board[j][i]
+                if state[j][i] == 0: return False
+                sum_column += state[j][i]
             if sum_column != expect: return False
 
         # Check all sub square
@@ -62,51 +62,62 @@ class Board:
                     square_sum = 0
                     for sub_row in range(0, self.square_size):
                         for sub_column in range(0, self.square_size):
-                            square_sum += self.board[sub_row][sub_column]
+                            square_sum += state[sub_row][sub_column]
                     if square_sum != expect: return False
         return True
 
-    def find_empty_cell(self):
+    def find_empty_cell(self, state):
         for row in range(self.size):
             for col in range(self.size):
-                if self.board[row][col] == 0:
+                if state[row][col] == 0:
                     return row, col
         return None, None
 
-    def expand(self):
+    def filter_row(self, state, row, column):
+        pass
+
+    def filter_column(self, state, row, column):
+        pass
+
+    def filter_subsquare(self, state, row, column):
+        pass
+    
+    def expand(self, state, history):
         values = range(1, self.size + 1)
-        row, column = self.find_empty_cell()
+        row, column = self.find_empty_cell(state)
         
         if row == None or column == None: return None
         used = set()
         for i in range(0, self.size):
-            used.add(self.board[row][i])
-            used.add(self.board[i][column])
+            used.add(state[row][i])
+            used.add(state[i][column])
         
         square_row_start = int(row / self.square_size) * self.square_size
         square_column_start = int(column / self.square_size) * self.square_size
         for i in range(0, self.square_size):
             for j in range(0, self.square_size):
-                used.add(self.board[square_row_start + i][square_column_start + j])
+                used.add(state[square_row_start + i][square_column_start + j])
         valid_value = [number for number in values if number not in used]
-        return_queue = [copy.deepcopy(self.board) for i in valid_value]
+        return_queue = [copy.deepcopy(state) for i in valid_value]
         for i in range(0, len(return_queue)):
             return_queue[i][row][column] = valid_value[i]
-            return_queue[i] = (return_queue[i], self.history + [(row, column, valid_value[i], self.board)])
+            return_queue[i] = (return_queue[i], history + [(row, column, valid_value[i], state)])
         return return_queue
     
     def bfs(self): 
         queue = [(copy.deepcopy(self.board), [])]
+        state = None
+        history = None
         while (len(queue) != 0):
-            self.board = queue[0][0]
-            self.history = queue[0][1]
+            state = queue[0][0]
+            history = queue[0][1]
             queue.pop(0)
-            if self.check_goal(): return self.board, self.history
-            new_queue = self.expand()
-            if new_queue == None: 
+            if self.check_goal(state): return state, history
+            expand_queue = self.expand(state, history)
+            if expand_queue == None: 
                 print("No valid move")
                 return
-            queue += new_queue
+            queue += expand_queue
 
 def online_init():
     level = -1
@@ -131,9 +142,9 @@ def custom_init():
 
 
 if __name__ == "__main__":
-    board = Board(custom_init(), "custom")
+    board = Sudoku(custom_init(), "custom")
     print(board)
-    board.bfs()
+    result, history = board.bfs()
     with open("sudoku_result.txt", "w") as output_file:
-        output_file.write(board.result())
+        output_file.write(Sudoku.result(result, history, board.size))
 
